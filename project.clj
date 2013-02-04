61c932aef365a50f1f4eaa27caec0dbcff2432d7
Jeff Palmucci <jpalmucci@csail.mit.edu>
2013-02-04 16:08:12 -0500

    remove snapshot tag

--- project.clj
+++ project.clj
@@ -1,14 +1,7 @@
-(defproject clj-return-from "1.0.1-SNAPSHOT"
+(defproject clj-return-from "1.0.1"
   :description "Emulate common lisps return-from for clojure"
-  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :java-source-path "src/java"
-  :dependencies [[org.clojure/tools.macro "0.1.2-SNAPSHOT"]]
-  :dev-dependencies [[lein-javac "1.2.1-SNAPSHOT"]
-                     [lein-multi "1.1.0-SNAPSHOT"]]
-  :multi-deps {"1.3" [[org.clojure/clojure "1.3.0-beta3"]
-                      [org.clojure/tools.macro "0.1.2-SNAPSHOT"]]
-               "1.2" [[org.clojure/clojure "1.2.1"]
-                      [org.clojure/tools.macro "0.1.2-SNAPSHOT"]]}
+  :dependencies [[org.clojure/tools.macro "0.1.1"]]
   )
 
 
