15f0a31b0744ea51e68407e2721e8ccb4d050af3
Jeff Palmucci <jpalmucci@csail.mit.edu>
2013-02-04 17:27:14 -0500

    fix an issue with lein2

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-return-from "1.0.1"
   :description "Emulate common lisps return-from for clojure"
-  :java-source-path "src/java"
+  :java-source-paths ["java"]
   :dependencies [[org.clojure/tools.macro "0.1.1"]]
   )
 
