e71280d93c9ea09779eacc9f28da3a69e4ede7b8
travis-rodman <travis@qcompute.com>
2016-02-11 18:41:18 -0800

    Update project.clj
    Updated the reference to tools.macro.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-return-from "1.0.1"
   :description "Emulate common lisps return-from for clojure"
   :java-source-paths ["java"]
-  :dependencies [[org.clojure/tools.macro "0.1.1"]]
+  :dependencies [[org.clojure/tools.macro "0.1.2"]]
   )
 
 
